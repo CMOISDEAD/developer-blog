8b4e05b74adfd1141635ca70d4d2ba7f60ea8fa3
CMOISDEAD <camilodavina@gmail.com>
2023-11-04 23:10:17 -0500

    feat: books page added

---
+++ src/pages/data.ts
@@ -0,0 +1,62 @@
+const data = [
+  {
+    category: "Computer cience",
+    books: [
+      {
+        title: "Clean Code",
+        description: "A Handbook of Agile Software Craftsmanship",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/41jEbK-jG+L.jpg",
+      },
+      {
+        title: "The Art of Computer Programming",
+        description: "Fundamental Algorithms",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/41jEbK-jG+L.jpg",
+      },
+    ],
+  },
+  {
+    category: "Mathematics/Physics",
+    books: [],
+  },
+
+  {
+    category: "Philosophy",
+    books: [],
+  },
+  {
+    category: "Poetry",
+    books: [],
+  },
+  {
+    category: "Religion",
+    books: [],
+  },
+  {
+    category: "Politics Science",
+    books: [],
+  },
+  {
+    category: "History",
+    books: [],
+  },
+  {
+    category: "Science",
+    books: [],
+  },
+  {
+    category: "Fiction",
+    books: [],
+  },
+  {
+    category: "Non-Fiction",
+    books: [],
+  },
+  {
+    category: "Other",
+    books: [],
+  },
+];
+
+export default data;
